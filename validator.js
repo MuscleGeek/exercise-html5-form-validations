507734b6011beb82e9d7b5e221c9f1b4c852a4db
Jonathan Villalobos Mora <hackertristania@gmail.com>
2021-01-30 06:17:59 +0000

    working @validz

--- validator.js
+++ validator.js
@@ -18,21 +18,73 @@
 */
 window.onload = () => {
         let getForm = document.querySelector('#myForm'); //id formulario
-        let showResult = document.querySelector("#showResult") //muestra la validación de los datos ingresados.
+        let showResult = document.querySelector("#showResult"); //muestra la validación de los datos ingresados.
 
         getForm.addEventListener('submit', function()
         {
-            //get input ids
-        let myTarjeta = document.querySelector('#Tarjeta').value;
-            if (!/^([0-9])*$/.test(myTarjeta)){
+                            /******FORM => BUTTON => SUBMIT */
+
+        /****************INPUT CARD */
+        let myTarjeta = document.querySelector('#tarjeta').value;
+            let cardRegex = !/^([0-9])*$/;
+            if (!cardRegex.test(myTarjeta)){
                 alert("El valor " + myTarjeta + " no es un número");
+                document.querySelector(myTarjeta).focus();
+            }else
+            {
+                alert('La tarjeta ingresada es valida');
             }
-                
-        let myCVC =     document.querySelector('#CVC').value;
-        let myMonto =   document.querySelector('#Monto').value;
-        let myNombre =  document.querySelector('#Nombre').value;
-        let myApellidos = document.querySelector('#Apellido').value;
+
+         /***************INPUT CVC */       
+        let myCVC = document.querySelector('#cvc').value;
+            let regex = /^[0-9]{3,4}$/;
+            if(!regex.test(myCVC)){
+                alert('El CVC ingresado no es valido!')
+                document.querySelector('#cvc').focus();
+            }else{
+                alert('El CVC ingresado es valido');
+            }
+        /****************INPUT MOUNT */
+
+        let myMount =   document.querySelector('#monto').value;
+        /****************INPUT NAME */
+            let mountRegex = /^[1-9]\d*(?:\.\d{9,2})?$/;
+            if(!mountRegex.test(myMount)){
+                alert('El monto ingresado es invalido');
+                document.querySelector('#monto').focus();
+            }else
+                alert('El monto a sido validado');
+
+        let myName =  document.querySelector('#nombre').value;
+            let nameVal = /^[a-zA-Z]+ [a-zA-Z]+$/;
+            if(!nameVal.test(myName)){
+                alert('El nombre suministrado no es valido!');
+                document.querySelector('#nombre').focus();
+            }else
+            {
+                alert('Su nombre es valido!');
+            }
+
+        /*****************INPUT LASTNAME */
+        let myApellido = document.querySelector('#apellido').value;
+            let lastnameVal = /^[a-zA-Z]+ [a-zA-Z]+$/;
+            if(!lastnameVal.test(myApellido)){
+                alert('Los apellidos intresados no son validos!');
+                document.querySelector('#apellido').focus;
+            }else
+            {
+                alert('Los apellidos ingresados son validos!');
+            }
+        /****************INPUT CITY */    
         let myCiudad = document.querySelector('#Ciudad').value;
+            let ciudadRegex = /^[a-zA-Z]+ [a-zA-Z]+$/;
+            if(!ciudadRegex.test('myCiudad')){
+                alert('El dato ingresado es invalido');
+                document.querySelector('#ciudad').focus();
+            }else
+            {
+                alert('El dato ingresado es valido');
+            }
         
         let myProvincia = document.querySelector('#Provincia');
             let optionSelIndexValue = myProvincia.options[myProvincia.selectedIndex].value;
@@ -42,8 +94,15 @@ window.onload = () => {
                 }else{
                     alert("Hecho! Usted a selecccionado:"+ optionSelIndexText);
                 }
-                
-        let myZip = document.querySelector('#Zip');
+        /****************INPUT ZIPCODE */        
+        let myZip = document.querySelector('#zip');
+            let zipRegex = /^[0-9]+$/;
+            if(myZip.length  == 5 && !zipRegex.test(myZip)){
+                alert('El dato suministrado es invalido')
+                document.querySelector('#zip').focus()
+            }else{
+                alert('El dato ingresado es valido');
+            }
         });
         
 
